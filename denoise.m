ef9d86a9ea3b8b48022514218a00f9b2a8dc4d5c
OliveG2 <56808357+OliveG2@users.noreply.github.com>
2019-11-22 18:22:40 -0500

    Add files via upload

---
+++ denoise.m
@@ -0,0 +1,28 @@
+function denoised = denoise(img)
+
+[x, y] = meshgrid(-10:10); % Build square matrices with the specified range
+
+% Standard deviation of the blur;
+sigma = 2;
+blur = exp(-(x.^2 + y.^2)/(2*sigma^2)); % Gaussian blur -- ran out of time!
+blur = blur / sum(blur(:)); % Normalize to 1
+
+img_cc_denoise = zeros(size(img, 1),size(img, 2),3,'double');
+img_cc_denoise(:,:,1) = filter2(blur, img(:,:,1));
+img_cc_denoise(:,:,2) = filter2(blur, img(:,:,2));
+img_cc_denoise(:,:,3) = filter2(blur, img(:,:,3));
+
+%img_cc_denoise = sigdenoise(img_cc_denoise);
+
+denoised = img_cc_denoise;
+
+% also denoises in frequnecy domain:
+
+b = firpm(10,[0 0.4 0.6 1],[1 1 0 0])
+h = ftrans2(b);
+[H,w] = freqz(b,1,64,'whole');
+
+% Shows the filter in a 3d plot: 
+% colormap(jet(64))
+% plot(w/pi-1,fftshift(abs(H)))
+% figure, freqz2(h,[32 32])2 32]), axis([-1 1 -1 1 0 1.2])
--- isp_demosaic.m
+++ isp_demosaic.m
@@ -12,8 +12,8 @@ rgb = cat(3, red, green, blue);
 
 % Do cubic interpolation for each color channel
 rgb_interp = zeros(size(img, 1)-1,size(img, 2)-1,3,'double');
-rgb_interp(:,:,1) = interp2(rgb(:,:,1), 1, 'cubic');
-rgb_interp(:,:,2) = interp2(rgb(:,:,2), 1, 'cubic');
-rgb_interp(:,:,3) = interp2(rgb(:,:,3), 1, 'cubic');
+rgb_interp(:,:,1) = interp2(rgb(:,:,1), 1, 'spline');
+rgb_interp(:,:,2) = interp2(rgb(:,:,2), 1, 'spline');
+rgb_interp(:,:,3) = interp2(rgb(:,:,3), 1, 'spline');
 
 
