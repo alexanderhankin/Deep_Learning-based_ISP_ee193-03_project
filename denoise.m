1efe59baf25dcf689f315d2449ae677d7a432ee7
acolin02 <32846947+acolin02@users.noreply.github.com>
2019-12-15 14:43:14 -0500

    Add files via upload

--- denoise.m
+++ denoise.m
@@ -1,28 +1,45 @@
 function denoised = denoise(img)
 
-[x, y] = meshgrid(-10:10); % Build square matrices with the specified range
+% For psnr are we using a special matlab function, or the normal psnr
+% function??
 
-% Standard deviation of the blur;
-sigma = 2;
-blur = exp(-(x.^2 + y.^2)/(2*sigma^2)); % Gaussian blur -- ran out of time!
-blur = blur / sum(blur(:)); % Normalize to 1
+ % DeepNetwork Denoise
+% % Took 30 min, but it worked!
+% [noisyR,noisyG,noisyB] = imsplit(img);
+% 
+% net = denoisingNetwork('DnCNN');
+% r_denoised = denoiseImage(noisyR,net);
+% g_denoised = denoiseImage(noisyG,net);
+% b_denoised =  denoiseImage(noisyB,net);
+% 
+% denoised = cat(3,r_denoised,g_denoised,b_denoised);
 
-img_cc_denoise = zeros(size(img, 1),size(img, 2),3,'double');
-img_cc_denoise(:,:,1) = filter2(blur, img(:,:,1));
-img_cc_denoise(:,:,2) = filter2(blur, img(:,:,2));
-img_cc_denoise(:,:,3) = filter2(blur, img(:,:,3));
+%rggb, panasonic #15,
+%cannon #29
 
-%img_cc_denoise = sigdenoise(img_cc_denoise);
+ 
 
-denoised = img_cc_denoise;
 
-% also denoises in frequnecy domain:
 
-b = firpm(10,[0 0.4 0.6 1],[1 1 0 0])
-h = ftrans2(b);
-[H,w] = freqz(b,1,64,'whole');
+% % Normal Denoise
+% [x, y] = meshgrid(-1:1); % Build square matrices with the specified range
+% 
+% % Standard deviation of the blur;
+% sigma = 2;
+% blur = exp(-(x.^2 + y.^2)/(2*sigma^2)); % Gaussian blur -- ran out of time!
+% blur = blur / sum(blur(:)); % Normalize to 1
+% 
+% img_cc_denoise = zeros(size(img, 1),size(img, 2),3,'double');
+% img_cc_denoise(:,:,1) = filter2(blur, img(:,:,1));
+% img_cc_denoise(:,:,2) = filter2(blur, img(:,:,2));
+% img_cc_denoise(:,:,3) = filter2(blur, img(:,:,3));
+% 
+% %img_cc_denoise = sigdenoise(img_cc_denoise);
+% 
+% denoised = imsharpen(rescale(img_cc_denoise));
 
-% Shows the filter in a 3d plot: 
-% colormap(jet(64))
-% plot(w/pi-1,fftshift(abs(H)))
-% figure, freqz2(h,[32 32])2 32]), axis([-1 1 -1 1 0 1.2])
+
+% Wavelate denoise, it works too!
+  denoised = wdenoise2(img);
+
+
